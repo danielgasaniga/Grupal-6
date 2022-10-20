2f2c6e1cc7b6e98324ba932c2d46ba79e5a8cc4c
FabianMontossi <110575173+FabianMontossi@users.noreply.github.com>
2022-10-19 21:10:50 -0300

    Almost done

--- registro2/js/validacion.js
+++ registro2/js/validacion.js
@@ -1,25 +1,33 @@
-/* let form = document.querySelector("form")
-let name = document.getElementById("nombre")
-let lastName = document.getElementById("apellido")
-let email = document.getElementById("email")
-let contraseña = document.getElementById("password1")
-let repetirContraseña = document.getElementById("password2")
-let terminos = document.getElementById("terminos")
-const registrarme = document.getElementById("registrarme")
+/*let form = document.querySelector("form");
+let name = document.getElementById("nombre");
+let lastName = document.getElementById("apellido");
+let email = document.getElementById("email");
+let terminos = document.getElementById("terminos");
+const registrarme = document.getElementById("registrarme");*/
+  
+const forms = document.querySelectorAll('.needs-validation');
+let password = document.getElementById("password1");
+let repeatPassword = document.getElementById("password2");
 
-form.addEventListener("submit", function(e){
-    e.preventDefault()
-    if(name.value.length === 0){
-        icono_name.classList.add('icono_error');
-        text_name.classList.add("text");
-        name.style.border="0.5px solid red";
-        console.log(form)
-    }
-}) */
+let realCheck = document.getElementById("terminos");
+let fakeCheck = document.getElementById("input-check");
 
+function checkTyC(){
+  if (realCheck.checked === true && !fakeCheck.checked){
+    fakeCheck.checked = true;
+  } else{
+    fakeCheck.checked = false;
+  }
+  console.log(fakeCheck.checked);
+ /* was-validated
+  is-invalid*/
+}
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
     'use strict'
-  
+
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.needs-validation')
   
@@ -30,7 +38,22 @@ form.addEventListener("submit", function(e){
           event.preventDefault()
           event.stopPropagation()
         }
-  
+
+      if ( (password.value.length > 5 && repeatPassword.length > 5) && (repeatPassword.value === password.value) ){
+        password.classList.remove("is-invalid");
+        password.classList.add('was-validated') 
+
+        repeatPassword.classList.remove("is-invalid");
+        repeatPassword.classList.add('was-validated')
+
+      }
+      else {
+
+          password.classList.add('is-invalid')
+          repeatPassword.classList.add('is-invalid')
+      }
+      console.log("password.value.length > 5 && repeatPassword.length > 5", password.value.length > 5 && repeatPassword.length > 5, "\nrepeatPassword.value === password.value", repeatPassword.value === password.value);
+
         form.classList.add('was-validated')
       }, false)
     })
